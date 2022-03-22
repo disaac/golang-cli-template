5174c517946f3387e1079e4280f7eb8856c199dc
Daniel <daniel@sdunixgeek.com>
2022-03-22 08:29:41 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sdunixgeek/golang-cli-template
+
+go 1.15
+
+require (
+	github.com/spf13/cobra v1.1.3
+	github.com/stretchr/testify v1.3.0
+)
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+read -rp "GitHub Username: " user
+read -rp "Projectname: " projectname
+
+git clone git@github.com:sdunixgeek/golang-cli-template.git "$projectname"
+cd "$projectname"
+rm -rf .git
+find . -type f -exec sed -i "s/golang-cli-template/$projectname/g" {} +
+find . -type f -exec sed -i "s/sdunixgeek/$user/g" {} +
+git init
+git add .
+git commit -m "initial commit"
+git remote add origin "git@github.com:$user/$projectname.git"
+
+echo "template successfully installed."
